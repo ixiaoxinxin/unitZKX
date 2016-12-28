3647e72d1b4337692c525bd7939393a69b091c41
Anniez <295563386@qq.com>
2016-12-28 15:35:39 +0800

    12.28

--- src/test/suit/run.py
+++ src/test/suit/run.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import executer
 import config
 import testcase
+
+from src.test.common import executer
 
 # executer.run(conf.MSWindows, testcase.validations.TestCase001_NormalInputTest)
 # executer.run(conf.ChromeDemo, testcase.validations.TestCase002_Data_In_Excel)
