1c42df6f99657cc2654c2bae80d935d5d2a83ae5
Anniez <295563386@qq.com>
2017-01-10 17:25:21 +0800

    1.10

--- example/demoprj/testcase/feeTest.py
+++ example/demoprj/testcase/feeTest.py
@@ -2,11 +2,15 @@
 
 from example.demoprj.page import feePage
 from ixiaoxinxin import Basepage
+import time
 
 def TestCase001():
     feePage.Login.username.Set("sxxykhjl")
+    time.sleep(2)
     feePage.Login.passwd.Set("111111")
-    feePage.Login.username.Click()
+    time.sleep(2)
+    feePage.Login.ok.Click()
+    time.sleep(2)
     Basepage.WebBrowser.SwitchToFrame(feePage.changeframes.changeframe1.value)
     feePage.register.hygl.Click()
     feePage.register.hyzc.Click()
